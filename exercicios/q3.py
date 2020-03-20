278babe6671a6b5a80467da7c06ab6492cde4c41
PedroCeruti <pedrocerute@gmail.com>
2020-03-20 02:14:55 +0000

    criação de arquivo com descrição dos programas

--- exercicios/q3.py
+++ exercicios/q3.py
@@ -1,3 +1,5 @@
-r = input("Digite o raio: ")
-pi = 3,14159
-area = 
+r = int(input("Digite o raio: "))
+#pi = (3,14159)
+#area = pi * r
+area = 3,14 * (r**2)
+print(area)
---
+++ exercicios/q4.py
@@ -0,0 +1,3 @@
+a = float(input("Digite o valor do lado: "))
+area = a * a
+print(area * 2)
