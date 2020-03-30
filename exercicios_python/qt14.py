310eb4679432da9ff041bf1d6b970174f031782d
PedroCeruti <pedrocerute@gmail.com>
2020-03-30 17:44:53 +0000

    Exercícios

--- exercicios_python/qt14.py
+++ exercicios_python/qt14.py
@@ -3,11 +3,18 @@ m2 = float(input("Digite o tamanho da área a ser pintada: "))
 l = m2/6
 la = l/18
 g = l/3.6
-if(l >= 18):
-  v = la * 80
-  print(f"Vão ser necessarias {la} latas")
-  print(f"Valor total: R${v:.2f}")
-if(l >= 3.6) and (l <= 17):
-  vg = g * 25
-  print(f"Vão ser necessarias {g} galões")
-  print(f"Valor total: R${vg:.2f}")
+qtdel = 0
+v = la * 80
+print(f"Vão ser necessarias {la:.1f} latas")
+print(f"Valor total: R${v:.2f} \n")
+vg = g * 25
+print(f"Vão ser necessarias {g} galões")
+print(f"Valor total: R${vg:.2f} \n")
+'''
+if((l >= 3.6) and (l <= 17) or (l >= 18)):
+  if(la != 0):
+    qtdel += 1
+  vt = v + vg
+  print(f"Vão ser necessários {g} galões e {qtdel} latas")
+  print(f"Valor total: R${vt:.2f}")
+'''
